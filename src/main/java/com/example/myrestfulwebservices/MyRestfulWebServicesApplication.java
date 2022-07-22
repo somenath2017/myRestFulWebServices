4074b8b6bcb9ed38808154d2c8dd506f5d446837
Somenath Chatterjee <somnath07in@gmail.com>
2022-07-22 16:42:44 +0530

    Initial Commit

---
+++ src/main/java/com/example/myrestfulwebservices/MyRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestfulWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRestfulWebServicesApplication.class, args);
+    }
+
+}
